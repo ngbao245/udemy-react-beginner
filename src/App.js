ce515195c599a01084acb4e145cb3274d70f31db
BaoBiBo <ng.hoangbao03@gmail.com>
2024-04-23 18:44:10 +0700

    Prepare Library

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Header from "./components/Header";
+import TableUsers from "./components/TableUsers";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world React with Hoi Dan IT
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Header />
+      <TableUsers />
     </div>
   );
 }
---
+++ src/components/TableUsers.js
@@ -0,0 +1,5 @@
+const TableUsers = (props) => {
+    return (<>TableUsers...</>);
+}
+
+export default TableUsers;
