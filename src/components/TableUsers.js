d58a375e7a9b2c7e72a1e9e10ddb2c57eb954d00
BaoBiBo <ng.hoangbao03@gmail.com>
2024-04-25 23:28:21 +0700

    add axios-customize

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -13,10 +13,9 @@ const TableUsers = (props) => {
   const getUsers = async () => {
     let res = await fetchAllUser();
 
-    if (res && res.data && res.data.data) {
-      setListUsers(res.data.data);
+    if (res && res.data) {
+      setListUsers(res.data);
     }
-    // console.log(">>> check response: ", res);
   };
   console.log(listUsers);
 
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,7 +1,8 @@
-import axios from "axios";
+// import axios from "axios";
+import axios from "./Customize-Axios";
 
 const fetchAllUser = () => {
-  return axios.get("https://reqres.in/api/users?page=1");
+  return axios.get("/api/users?page=1");
 };
 
 export { fetchAllUser };
