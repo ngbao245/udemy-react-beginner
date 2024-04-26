539bbcf857032e2a1afc72c1b7927b1abf2ff747
BaoBiBo <ng.hoangbao03@gmail.com>
2024-04-26 16:20:08 +0700

    add pagination

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,48 +1,79 @@
 import { useEffect, useState } from "react";
 import Table from "react-bootstrap/Table";
 import { fetchAllUser } from "../services/UserService";
+import ReactPaginate from "react-paginate";
 
 const TableUsers = (props) => {
   const [listUsers, setListUsers] = useState([]);
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
     //call apis
-    getUsers();
+    getUsers(1);
   }, []);
 
-  const getUsers = async () => {
-    let res = await fetchAllUser();
+  const getUsers = async (page) => {
+    let res = await fetchAllUser(page);
 
     if (res && res.data) {
+      setTotalUsers(res.total)
       setListUsers(res.data);
+      setTotalPages(res.total_pages)
     }
   };
-  console.log(listUsers);
+
+  const handlePageClick = (event) => {
+    console.log("event lib: ", event);
+    getUsers(+event.selected+1);
+  };
 
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Email</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-        </tr>
-      </thead>
-      <tbody>
-        {listUsers && listUsers.length > 0 && 
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listUsers &&
+            listUsers.length > 0 &&
             listUsers.map((item, index) => {
-            return (
-              <tr key={`users-${index}`}>
-                <td>{item.id}</td>
-                <td>{item.email}</td>
-                <td>{item.first_name}</td>
-                <td>{item.last_name}</td>
-              </tr>
-            );
-          })}
-      </tbody>
-    </Table>
+              return (
+                <tr key={`users-${index}`}>
+                  <td>{item.id}</td>
+                  <td>{item.email}</td>
+                  <td>{item.first_name}</td>
+                  <td>{item.last_name}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        pageCount={totalPages}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+      />
+    </>
   );
 };
 
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,8 +1,8 @@
 // import axios from "axios";
 import axios from "./Customize-Axios";
 
-const fetchAllUser = () => {
-  return axios.get("/api/users?page=1");
+const fetchAllUser = (page) => {
+  return axios.get(`/api/users?page=${page}`);
 };
 
 export { fetchAllUser };
