944045f4e684b4b20af84fac3b21725bff8aa810
BaoBiBo <ng.hoangbao03@gmail.com>
2024-05-09 23:12:12 +0700

    Add ErrorBoundary

--- src/index.js
+++ src/index.js
@@ -8,12 +8,15 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store";
 import { Provider } from "react-redux";
+import ErrorBoundary from "./components/ErrorBoundary/Error";
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>,
